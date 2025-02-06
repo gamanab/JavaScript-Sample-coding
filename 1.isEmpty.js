4efe09f2da993282183e50bf5f7a7656d2ef3f8e
Vasanth Venu <gamanabandapalli@gmail.com>
2025-02-06 21:08:15 +0530

    Modified the 1.isEmpty.js

--- 1.isEmpty.js
+++ 1.isEmpty.js
@@ -7,9 +7,9 @@
 // function declaration ( Named function )
 function empty(a) {
     if (a === '') {
-        return `true`;
+        return true;
     } else {
-        return `false`;
+        return false;
     }
 }
 console.log(empty(""));
@@ -20,35 +20,34 @@ console.log(empty(123));
 // Function Expression (Anonymous Function)
 const empty1 = function (a1) {
     if (a1 === '') {
-        return `true`;
+        return true;
     } else {
-        return `false`;
+        return false;
     }
 }
-console.log(empty(""));
-console.log(empty(" "));
-console.log(empty("hi"));
-console.log(empty(123));
+console.log(empty1(""));
+console.log(empty1(" "));
+console.log(empty1("hi"));
+console.log(empty1(123));
 
 //Arrow Function (Explicit Return)
 const empty2 = (a2) => {
     if (a2 === '') {
-        return `true`;
+        return true;
     } else {
-        return `false`;
+        return false;
     }
 }
-console.log(empty(""));
-console.log(empty(" "));
-console.log(empty("hi"));
-console.log(empty(123));
+console.log(empty2(""));
+console.log(empty2(" "));
+console.log(empty2(123));
 
 //Arrow function (Implicit Return - Shorter Syntax)
-const empty3 = (a3) => a3 === "" ? `true` : `FALSE`;
-console.log(empty(""));
-console.log(empty(" "));
-console.log(empty("hi"));
-console.log(empty(123));
+const empty3 = (a3) => a3 === "" ? true : false;
+console.log(empty3(""));
+console.log(empty3(" "));
+console.log(empty3("hi"));
+console.log(empty3(123));
 
 
 
