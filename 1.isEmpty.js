ed66b7bee51db7229b469d61e68dd1889a9123e8
Vasanth Venu <gamanabandapalli@gmail.com>
2025-02-06 20:40:38 +0530

    Update the 1.isEmpty.js

--- 1.isEmpty.js
+++ 1.isEmpty.js
@@ -4,14 +4,51 @@
 //           empty('hi') -> false
 
 
-// Named function
+// function declaration ( Named function )
 function empty(a) {
-    if (a === '')
+    if (a === '') {
         return `true`;
-    else
+    } else {
         return `false`;
+    }
 }
 console.log(empty(""));
 console.log(empty(" "));
 console.log(empty("hi"));
 console.log(empty(123));
+
+// Function Expression (Anonymous Function)
+const empty1 = function (a1) {
+    if (a1 === '') {
+        return `true`;
+    } else {
+        return `false`;
+    }
+}
+console.log(empty(""));
+console.log(empty(" "));
+console.log(empty("hi"));
+console.log(empty(123));
+
+//Arrow Function (Explicit Return)
+const empty2 = (a2) => {
+    if (a2 === '') {
+        return `true`;
+    } else {
+        return `false`;
+    }
+}
+console.log(empty(""));
+console.log(empty(" "));
+console.log(empty("hi"));
+console.log(empty(123));
+
+//Arrow function (Implicit Return - Shorter Syntax)
+const empty3 = (a3) => a3 === "" ? `true` : `FALSE`;
+console.log(empty(""));
+console.log(empty(" "));
+console.log(empty("hi"));
+console.log(empty(123));
+
+
+
